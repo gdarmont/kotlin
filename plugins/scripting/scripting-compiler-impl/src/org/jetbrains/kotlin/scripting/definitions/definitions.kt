1d90fc37d45cd129207f9b0ad2cb1c47f8f671bb
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-11-19 14:02:04 +0100

    Change script definition lookup key from File to URI
    to make it more generic and allow easier implementation fo the
    non-file based scripts.

--- plugins/scripting/scripting-compiler-impl/src/org/jetbrains/kotlin/scripting/definitions/definitions.kt
+++ plugins/scripting/scripting-compiler-impl/src/org/jetbrains/kotlin/scripting/definitions/definitions.kt
@@ -16,7 +16,7 @@ import com.intellij.psi.PsiManager
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.parsing.KotlinParserDefinition
 import org.jetbrains.kotlin.psi.KtFile
-import java.io.File
+import java.net.URI
 
 inline fun <T> runReadAction(crossinline runnable: () -> T): T {
     return ApplicationManager.getApplication().runReadAction(Computable { runnable() })
@@ -29,7 +29,7 @@ fun PsiFile.findScriptDefinition(): ScriptDefinition? {
     val virtualFile = this.virtualFile ?: this.originalFile.virtualFile ?: return null
     if (virtualFile.isNonScript()) return null
 
-    return findScriptDefinitionByFilePath(project, File(virtualFile.path))
+    return findScriptDefinitionById(project, URI(virtualFile.url))
 }
 
 @Deprecated("Use PsiFile.findScriptDefinition() instead")
@@ -41,14 +41,14 @@ fun VirtualFile.findScriptDefinition(project: Project): ScriptDefinition? {
     // TODO: measure performance effect and if necessary consider detecting indexing here or using separate logic for non-IDE operations to speed up filtering
     if (runReadAction { PsiManager.getInstance(project).findFile(this) as? KtFile }/*?.script*/ == null) return null
 
-    return findScriptDefinitionByFilePath(project, File(path))
+    return findScriptDefinitionById(project, URI(url))
 }
 
-private fun findScriptDefinitionByFilePath(project: Project, file: File): ScriptDefinition {
+fun findScriptDefinitionById(project: Project, scriptId: URI): ScriptDefinition? {
     val scriptDefinitionProvider = ScriptDefinitionProvider.getInstance(project) ?: return null
         ?: throw IllegalStateException("Unable to get script definition: ScriptDefinitionProvider is not configured.")
 
-    return scriptDefinitionProvider.findDefinition(file) ?: scriptDefinitionProvider.getDefaultDefinition()
+    return scriptDefinitionProvider.findDefinition(scriptId) ?: scriptDefinitionProvider.getDefaultDefinition()
 }
 
 fun VirtualFile.isNonScript(): Boolean =
